9164b261c8f0c6501a692837e195830eba0e64e1
jtanyag <jtanyag@gmail.com>
2017-09-26 14:01:49 -0700

    Update CatProfile.js
    Updated componentWillMount and render

--- src/pages/CatProfile.js
+++ src/pages/CatProfile.js
@@ -9,40 +9,38 @@ import {
 class CatProfile extends Component {
 	constructor(props) {
 		super(props)
-
 		this.state = {
-			cat: undefined
+			apiURL: "http://localhost:3000",
+			id: this.props.location.pathname
 		}
 	}
 
 	componentWillMount() {
-		// this.props.match.params.id // cat ID
-		// fetch cat based on ID
-		const params = this.props.match.params.id
+		fetch(`${this.state.apiUrl}${this.state.id}`)
+		.then((rawResponse)=>{
+			return rawResponse.json()
+		})
+		.then((jsonresp)=>{
+			this.setState(
+				{
+					name: jsonresp[0].name,
+					age: jsonresp[0].age,
+					enjoys: jsonresp[0].enjoys,
+					city: jsonresp[0].city
+				}
+			)
+		})
+	}
 
-		fetch(`${this.state.apiUrl}/cats`,
-	      {
-	        headers: {  // <- We specify that we're sending JSON, and expect JSON back
-	          'Content-Type': 'application/json'
-	        },
-	        method: "POST"  // <- Here's our verb, so the correct endpoint is invoked on the server
-	      }
-	    )
-	    .then((params)=>{
-	      return params.json()
-	    })
-
-		   console.log(params);
-	    }
 
 	render() {
-		const { cat } = this.state
-
-		if(cat === undefined) {
-			return (<h1 className="wrangling">Wrangling Cats...</h1>)
-		} else {
-			return (<h1>{cat.id}</h1>)
-		}
+		return (
+			<div>
+			<p>Name: {this.state.name}</p>
+			<p>Age: {this.state.age}</p>
+			<p>City: {this.state.city}</p>
+			<p>Enjoys: {this.state.enjoys}</p>
+		)
 	}
 }
 
